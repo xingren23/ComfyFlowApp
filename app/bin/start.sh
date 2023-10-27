602718bbeec47cc1be85dac10f82e95073af5998
xingren23 <yexingren23@126.com>
2023-10-27 21:21:30 +0800

    app bin

---
+++ app/bin/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# get current path
+CUR_DIR="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
+PROJECT_PATH=$(dirname $(dirname "$CUR_DIR"))
+echo "$PROJECT_PATH"
+cd $PROJECT_PATH
+
+# set LOGURU_LEVEL to ERROR
+export LOGURU_LEVEL=DEBUG
+
+# app name
+APP_NAME="Apps"
+# script params
+nohup streamlit run "$APP_NAME.py" > creator.log 2>&1 &
+
+echo "Starting $APP_NAME..."
