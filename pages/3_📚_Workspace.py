3d5a4f65fdde3ae4b8a77dd581d733c149dd4829
wangzg <yexingren23@126.com>
2023-12-13 11:33:22 +0800

    fix: export disabled

--- pages/3_📚_Workspace.py
+++ pages/3_📚_Workspace.py
@@ -219,7 +219,8 @@ def create_operation_ui(app):
         if app_preview_ret == AppStatus.ERROR.value:
             st.error(f"Edit app {name} failed, please check the log")
 
-    if 'workflow_conf' in app:
+    logger.info(f"app {name} workflow_conf: {app.workflow_conf}")
+    if app.workflow_conf is not None:
         operate_row.download_button("💾 Export", data=app.workflow_conf, file_name=f"{app.name}_workflow.json", help="Export workflow to json", key=f"{id}-button-export",
                         disabled=disabled)
     else:
