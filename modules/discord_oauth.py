dcea7f163c80992ac1ba3eb8b3cd30dd0afd7626
wangzg <yexingren23@126.com>
2023-12-20 16:26:47 +0800

    enableCORS = false lazy import discordoauth2

--- modules/discord_oauth.py
+++ modules/discord_oauth.py
@@ -1,33 +1,45 @@
 import os
 from loguru import logger
-import discordoauth2
+
 import streamlit as st
 
 client_id = os.getenv('DISCORD_CLIENT_ID')
 client_secret = os.getenv('DISCORD_CLIENT_SECRET')
 redirect_uri = os.getenv('DISCORD_REDIRECT_URI')
 
-client = discordoauth2.Client(client_id, secret=client_secret, redirect=redirect_uri)
 
 @st.cache_resource
 def get_client():
-    return client
+    try:
+        import discordoauth2
+        client = discordoauth2.Client(client_id, secret=client_secret, redirect=redirect_uri)
+        return client
+    except Exception as e:
+        logger.error(f"get_client error: {e}")
+        return None
+   
 
 @st.cache_data(ttl=60 * 60)
 def gen_authorization_url():
     client = get_client()
-    authorization_url = client.generate_uri(scope=["identify", "email"])
-    logger.debug(f"authorization_url: {authorization_url}")
-    return authorization_url
+    if client:
+        authorization_url = client.generate_uri(scope=["identify", "email"])
+        logger.debug(f"authorization_url: {authorization_url}")
+        return authorization_url
+    else:
+        authorization_url = f"https://discord.com/oauth2/authorize?client_id={client_id}&scope=identify+email&redirect_uri={redirect_uri}&response_type=code"
+        logger.warning(f"discordoauth2 is none, default authorization_url : {authorization_url}")   
+        return authorization_url
 
 
 def get_user_data(code):
     try:
         client = get_client()
-        access = client.exchange_code(code)
-        user_data = access.fetch_identify()
-        logger.debug(f"user_data: {user_data}")
-        return user_data
+        if client:
+            access = client.exchange_code(code)
+            user_data = access.fetch_identify()
+            logger.debug(f"user_data: {user_data}")
+            return user_data
     except Exception as e:
         logger.error(f"get_user_data error: {e}")
         return None
