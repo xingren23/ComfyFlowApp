720f88388f2a7a1c1234a2820c3fe0f9f6ff3326
wangzg <yexingren23@126.com>
2024-03-21 21:24:18 +0800

    Fix app_table_name name

--- modules/myapp_model.py
+++ modules/myapp_model.py
@@ -21,9 +21,9 @@ my_apps table
 class MyAppModel:
     def __init__(self) -> None:
         self.db_conn = st.connection('comfyflow_db', type='sql')
-        self.app_talbe_name = 'my_apps'
+        self.app_table_name = 'my_apps'
         self._init_table()
-        logger.debug(f"db_conn: {self.db_conn}, app_talbe_name: {self.app_talbe_name}")
+        logger.debug(f"db_conn: {self.db_conn}, app_table_name: {self.app_table_name}")
 
     @property
     def session(self):
@@ -32,20 +32,20 @@ class MyAppModel:
     def _init_table(self):
         # Create a table if it doesn't exist.
         with self.session as s:
-            sql = text(f'CREATE TABLE IF NOT EXISTS {self.app_talbe_name} (id TEXT PRIMARY KEY, name TEXT, description TEXT, image TEXT, app_conf TEXT, api_conf TEXT, template TEXT, url TEXT, status TEXT, created_at TEXT, updated_at TEXT);')
+            sql = text(f'CREATE TABLE IF NOT EXISTS {self.app_table_name} (id TEXT PRIMARY KEY, name TEXT, description TEXT, image TEXT, app_conf TEXT, api_conf TEXT, template TEXT, url TEXT, status TEXT, created_at TEXT, updated_at TEXT);')
             s.execute(sql)
 
             # create index on name
-            sql = text(f'CREATE INDEX IF NOT EXISTS {self.app_talbe_name}_name_index ON {self.app_talbe_name} (name);')
+            sql = text(f'CREATE INDEX IF NOT EXISTS {self.app_table_name}_name_index ON {self.app_table_name} (name);')
             s.execute(sql)
             s.commit()
-            logger.info(f"init app table {self.app_talbe_name} and index")
+            logger.info(f"init app table {self.app_table_name} and index")
 
     def sync_apps(self, apps):
         
         with self.session as s:
             # reset local apps
-            sql = text(f'SELECT id, status FROM {self.app_talbe_name} where status!=:status order by id;')
+            sql = text(f'SELECT id, status FROM {self.app_table_name} where status!=:status order by id;')
             local_apps = s.execute(sql, dict(status=AppStatus.INSTALLED.value)).fetchall()
             delete_apps = []
             for app in local_apps:
@@ -55,7 +55,7 @@ class MyAppModel:
 
         # sync apps from comfyflow.app
         with self.session as s:
-            sql = text(f'SELECT id, status FROM {self.app_talbe_name} order by id;')
+            sql = text(f'SELECT id, status FROM {self.app_table_name} order by id;')
             local_apps = s.execute(sql).fetchall()
             local_app_ids = {app.id: app for app in local_apps}
             sync_apps = []
@@ -67,14 +67,14 @@ class MyAppModel:
                 if app['id'] in local_app_ids:
                     if local_app_ids[app['id']].status == AppStatus.PUBLISHED.value:
                         # update app
-                        sql = text(f'UPDATE {self.app_talbe_name} SET name=:name, description=:description, image=:image, template=:template, status=:status, updated_at=datetime("now") WHERE id=:id;')
+                        sql = text(f'UPDATE {self.app_table_name} SET name=:name, description=:description, image=:image, template=:template, status=:status, updated_at=datetime("now") WHERE id=:id;')
                         s.execute(sql, dict(id=app['id'], name=app['name'], description=app['description'], image=image_bytes, template=app['template'], status=AppStatus.PUBLISHED.value))
                         s.commit()
                         sync_apps.append(app['name'])
                         logger.info(f"update app {app['name']} to my apps")
                 else:
                     # insert app
-                    sql = text(f'INSERT INTO {self.app_talbe_name} (id, name, description, image, template, status, created_at, updated_at) VALUES (:id, :name, :description, :image, :template, :status, datetime("now"), datetime("now") );')
+                    sql = text(f'INSERT INTO {self.app_table_name} (id, name, description, image, template, status, created_at, updated_at) VALUES (:id, :name, :description, :image, :template, :status, datetime("now"), datetime("now") );')
                     s.execute(sql, dict(id=app['id'], name=app['name'], description=app['description'], image=image_bytes, template=app['template'], status=AppStatus.PUBLISHED.value))
                     s.commit()
                     sync_apps.append(app['name'])
@@ -85,7 +85,7 @@ class MyAppModel:
     def get_all_apps(self):
         with self.session as s:
             logger.info("get apps from db")
-            sql = text(f'SELECT id, name, description, image, app_conf, api_conf, endpoint, template, url, status, username FROM {self.app_talbe_name} order by id desc;')
+            sql = text(f'SELECT id, name, description, image, app_conf, api_conf, endpoint, template, url, status, username FROM {self.app_table_name} order by id desc;')
             apps = s.execute(sql).fetchall()
             return apps
         
@@ -93,56 +93,56 @@ class MyAppModel:
         # get installed apps
         with self.session as s:
             logger.debug("get my apps from db")
-            sql = text(f'SELECT id, name, description, image, app_conf, api_conf, template, url, status, username FROM {self.app_talbe_name} WHERE status=:status order by id desc;')
+            sql = text(f'SELECT id, name, description, image, app_conf, api_conf, template, url, status, username FROM {self.app_table_name} WHERE status=:status order by id desc;')
             apps = s.execute(sql, {'status': AppStatus.INSTALLED.value}).fetchall()
             return apps
 
     def get_app(self, name):
         with self.session as s:
             logger.debug(f"get app by name: {name}")
-            sql = text(f'SELECT * FROM {self.app_talbe_name} WHERE name=:name;')
+            sql = text(f'SELECT * FROM {self.app_table_name} WHERE name=:name;')
             app = s.execute(sql, {'name': name}).fetchone()
             return app
         
     def get_app_by_id(self, id):
         with self.session as s:
             logger.debug(f"get app by id: {id}")
-            sql = text(f'SELECT * FROM {self.app_talbe_name} WHERE id=:id;')
+            sql = text(f'SELECT * FROM {self.app_table_name} WHERE id=:id;')
             app = s.execute(sql, {'id': id}).fetchone()
             return app
         
     def delete_app(self, name):
         with self.session as s:
             logger.info(f"delete app: {name}")
-            sql = text(f'DELETE FROM {self.app_talbe_name} WHERE name=:name;')
+            sql = text(f'DELETE FROM {self.app_table_name} WHERE name=:name;')
             s.execute(sql, dict(name=name))
             s.commit()
 
     def delete_app_by_id(self, id):
         with self.session as s:
             logger.info(f"delete app: {id}")
-            sql = text(f'DELETE FROM {self.app_talbe_name} WHERE id=:id;')
+            sql = text(f'DELETE FROM {self.app_table_name} WHERE id=:id;')
             s.execute(sql, dict(id=id))
             s.commit()
 
     def update_app_status(self, id, status):
         with self.session as s:
             logger.info(f"update app status: {id}, {status}")
-            sql = text(f'UPDATE {self.app_talbe_name} SET status=:status WHERE id=:id;')
+            sql = text(f'UPDATE {self.app_table_name} SET status=:status WHERE id=:id;')
             s.execute(sql, dict(id=id, status=status))
             s.commit()
 
     def update_api_conf(self, id, api_conf):
         with self.session as s:
             logger.info(f"update app api_conf: {id}, {api_conf}")
-            sql = text(f'UPDATE {self.app_talbe_name} SET api_conf=:api_conf WHERE id=:id;')
+            sql = text(f'UPDATE {self.app_table_name} SET api_conf=:api_conf WHERE id=:id;')
             s.execute(sql, dict(id=id, api_conf=api_conf))
             s.commit()
 
     def update_app_conf(self, id, app_conf):
         with self.session as s:
             logger.info(f"update app app_conf: {id}, {app_conf}")
-            sql = text(f'UPDATE {self.app_talbe_name} SET app_conf=:app_conf WHERE id=:id;')
+            sql = text(f'UPDATE {self.app_table_name} SET app_conf=:app_conf WHERE id=:id;')
             s.execute(sql, dict(id=id, app_conf=app_conf))
             s.commit()
 
